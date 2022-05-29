60fff5c9c1bf557b5649f9a377bdd195a80fe075
伊语与雨 <826393025@qq.com>
2022-05-30 02:03:09 +0800

    Fix：点击0.0异常

--- voyager/control/mouse.py
+++ voyager/control/mouse.py
@@ -1,10 +1,18 @@
 import pyautogui
 
+pyautogui.FAILSAFE = False
+
 
 def moveTo(x, y):
+    if x == 0 and y == 0:
+        return
     pyautogui.moveTo(x, y, 0.2)
 
 
 def click(x, y):
     pyautogui.click(x, y)
 
+
+if __name__ == '__main__':
+    moveTo(0, 0)
+    click(0, 0)
