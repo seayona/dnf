48e265f4c1266690420fbe3a77394ddb5aedce97
伊语与雨 <826393025@qq.com>
2022-06-01 17:33:34 +0800

    F：成就奖励领取

--- voyager/workers/worker_welfare_achievement.py
+++ voyager/workers/worker_welfare_achievement.py
@@ -11,32 +11,60 @@ class WelfareAchievementWorker(QThread):
         super(WelfareAchievementWorker, self).__init__()
         self.voyager = voyager
         self.running = False
-        self.daily = {'one_key': False, box: False}
-        self.weekly = {'one_key': False, box: False}
+        self.daily = False
+        self.weekly = False
 
     def init(self):
         self.running = True
+        self.daily = False
+        self.weekly = False
 
     def _run(self):
         # 领取后还在每日返回
-        if not self.voyager.recogbot.town() and self.voyager.player.welfare['daily']:
+        if not self.voyager.recogbot.town() and self.voyager.player.welfare['achievement']:
             self.voyager.game.esc()
             return
 
         # 领取后在城镇stop
-        if self.voyager.recogbot.town() and self.voyager.player.welfare['daily']:
+        if self.voyager.recogbot.town() and self.voyager.player.welfare['achievement']:
             self.trigger.emit(str('stop'))
             return
 
-        # 没领取打开每日页面
-        if self.voyager.recogbot.town() and not self.voyager.player.welfare['daily']:
-            self.voyager.game.goto_mall_recovered_product()
+        # 没领取打开成就页面
+        if self.voyager.recogbot.town() and not self.voyager.player.welfare['achievement']:
+            self.voyager.game.goto_achievement()
             return
 
+        # 在每日页面
+        if self.voyager.recogbot.achievement_daily_active():
+            # 有还没领取的，进行领取
+            if self.voyager.recogbot.achievement_daily_box():
+                self.voyager.game.achievement_daily_box()
+                return
+            if self.voyager.recogbot.get_all():
+                self.voyager.game.achievement_get_all()
+                return
+            if self.voyager.recogbot.achievement_daily_sella():
+                self.voyager.game.achievement_daily_sella()
+                return
+            self.daily = True
 
+        # 每日领取完，去每周
+        if self.daily and self.voyager.recogbot.achievement_in_daily():
+            self.voyager.game.goto_achievement_weekly()
 
-        if True:
-            self.trigger.emit(str('stop'))
+        if self.voyager.recogbot.achievement_weekly_active():
+            # 每周奖励领取
+            if self.voyager.recogbot.get_all():
+                self.voyager.game.achievement_get_all()
+                return
+            if self.voyager.recogbot.weekly_box():
+                self.voyager.game.achievement_weekly_box()
+                return
+            self.weekly = True
+
+        if self.weekly and self.daily:
+            self.voyager.player.welfare['achievement'] = True
 
     def run(self):
         self.init()
